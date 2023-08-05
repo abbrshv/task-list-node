3daff0e800b2fa9da311fb67278d514a0641b35b
abbrshv <a.bobryshov@proton.me>
2023-08-05 23:38:59 +0200

    Fix routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,7 @@ import { Application } from 'express';
 import taskRoutes from './taskRoutes.js';
 
 const initRoutes = (app: Application) => {
-  app.use('/api/users', taskRoutes);
+  app.use('/notes', taskRoutes);
 };
 
 export default initRoutes;
