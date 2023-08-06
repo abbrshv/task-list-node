3c1670794e54304aac80e2ce4936c8b7e80f4335
abbrshv <a.bobryshov@proton.me>
2023-08-06 09:52:07 +0200

    Use strict mode, change response status code

--- src/middleware/validaton.middleware.ts
+++ src/middleware/validaton.middleware.ts
@@ -3,10 +3,10 @@ import { NextFunction, Request, Response } from 'express';
 
 const validate = (schema: Schema) => async (req: Request, res: Response, next: NextFunction) => {
   try {
-    await schema.validate(req.body);
+    await schema.validate(req.body, { strict: true });
     return next();
   } catch (error: any) {
-    return res.status(500).json({ type: error.name, message: error.message });
+    return res.status(403).json({ type: error.name, message: error.message });
   }
 };
 
