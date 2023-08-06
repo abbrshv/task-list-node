d9c467a681fc21607938628d826aa2d9a84938d1
abbrshv <a.bobryshov@proton.me>
2023-08-06 10:00:27 +0200

    Add unknown property and category validations

--- src/middleware/newTaskSchema.ts
+++ src/middleware/newTaskSchema.ts
@@ -1,11 +1,12 @@
 import { boolean, object, ObjectSchema, string } from 'yup';
+import { TASK_CATEGORIES, Category } from '../constants/taskCategories.js';
 
 const newTaskSchema: ObjectSchema<ETask> = object({
   name: string().min(3).required(),
-  category: string<'task' | 'random thought' | 'idea'>().required(),
+  category: string<Category>().oneOf(TASK_CATEGORIES).required(),
   content: string().min(3).required(),
   dates: string().nullable(),
   isArchived: boolean(),
-});
+}).noUnknown(true);
 
 export default newTaskSchema;
