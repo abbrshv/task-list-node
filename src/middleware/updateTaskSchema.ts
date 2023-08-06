ea27229054bc76e9906a2400821078cc40a957b7
abbrshv <a.bobryshov@proton.me>
2023-08-06 10:00:40 +0200

    Add category validations

--- src/middleware/updateTaskSchema.ts
+++ src/middleware/updateTaskSchema.ts
@@ -1,8 +1,9 @@
 import { boolean, date, object, ObjectSchema, string } from 'yup';
+import { TASK_CATEGORIES, Category } from '../constants/taskCategories.js';
 
 const updateTaskSchema: ObjectSchema<Partial<DTask>> = object({
   name: string().min(3),
-  category: string<'task' | 'random thought' | 'idea'>(),
+  category: string<Category>().oneOf(TASK_CATEGORIES),
   content: string().min(3),
   dates: string().nullable(),
   isArchived: boolean(),
