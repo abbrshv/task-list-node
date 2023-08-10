0c5c3507f258ed7ac041a3ea02cd24312f490f8c
abbrshv <a.bobryshov@proton.me>
2023-08-10 12:07:49 +0300

    Rewrite methods to work with sequelize

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,16 +1,11 @@
 import taskRepository from '../repositories/taskRepository.js';
 import { TASK_CATEGORIES } from '../constants/taskCategories.js';
-import CustomError from '../helpers/CustomError.js';
+import Task from '../database/models/Task.js';
 
 class TaskService {
-  isDTask(obj: any): obj is DTask {
-    return (
-      'name' in obj && 'category' in obj && 'content' in obj && 'createdDate' in obj && 'id' in obj
-    );
-  }
-
-  captureDates(task: ETask | DTask) {
+  captureDates(task: ETask | DTask | Partial<DTask> | Partial<ETask>) {
     const dateRegex = /(0?[1-9]|[12][0-9]|3[01])(\/|-)(0?[1-9]|1[1,2])(\/|-)(19|20)\d{2}/g;
+    if (!task.content) return null;
     const datesArray = task.content.match(dateRegex);
 
     return datesArray ? datesArray.join(', ') : null;
@@ -21,43 +16,35 @@ class TaskService {
   }
 
   get(id: string) {
-    const result = taskRepository.getOne(id);
-    if (result && Object.keys(result).length === 0 && result.constructor === Object) {
-      throw new CustomError(`Task with id: ${id} not found`, 404);
-    }
-    return result;
+    return taskRepository.getOne(id);
   }
 
   create(task: ETask) {
     const newTask = { ...task, isArchived: false, dates: this.captureDates(task) };
-    const result = taskRepository.create(newTask);
-    if (!result) throw new Error('Could not create task');
-
-    return result;
+    return taskRepository.create(newTask as Task);
   }
 
   update(id: string, updatedData: Partial<DTask>) {
-    const task = this.get(id);
-    if (!this.isDTask(task)) return null;
-
-    const { createdDate } = task;
-    const updatedTask: DTask = { ...task, ...updatedData, id: id, createdDate: createdDate };
-    updatedTask.dates = this.captureDates(updatedTask);
-
-    return taskRepository.update(updatedTask);
+    const updatedDataWithDates = { ...updatedData, dates: this.captureDates(updatedData) };
+    const result = taskRepository.update(id, updatedDataWithDates);
+    return result;
   }
 
   delete(id: string) {
-    return this.get(id) && taskRepository.delete(id);
+    const result = taskRepository.delete(id);
+    return result;
   }
 
-  getStats() {
-    const tasks = this.getAll();
+  async getStats() {
+    const tasks = await this.getAll();
+    const tasksUnwrapped = tasks.map((task) => task.get({ plain: true }));
 
     const stats: TaskStatObject[] = TASK_CATEGORIES.map((category) => ({
       categoryName: category,
-      active: tasks.filter((task) => task.category === category && !task.isArchived).length,
-      archived: tasks.filter((task) => task.category === category && task.isArchived).length,
+      active: tasksUnwrapped.filter((task) => task.category === category && !task.isArchived)
+        .length,
+      archived: tasksUnwrapped.filter((task) => task.category === category && task.isArchived)
+        .length,
     }));
 
     return stats;
